0f27e5aaf388f5c27b03d576b94662e442147851
Ubuntu <ubuntu@ip-172-31-37-161.eu-north-1.compute.internal>
2026-04-23 07:12:13 +0000

    updated deployfile

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-IMAGE_NAME=my-react-app
-DOCKERHUB_USER=dugyalaravali28
-TAG=dev
+# These variables make the script flexible
+IMAGE_NAME=$1
+DOCKERHUB_USER=$2
+TAG=$3
+DOCKER_PASS=$4
 
-docker pull $DOCKERHUB_USER/$IMAGE_NAME:$TAG
+# 1. Login to DockerHub (Required for the Private Prod repo)
+echo "$DOCKER_PASS" | docker login -u "$DOCKERHUB_USER" --password-stdin
 
-docker stop myapp || true
-docker rm myapp || true
+# 2. Pull the latest image
+docker pull "$DOCKERHUB_USER/$IMAGE_NAME:$TAG"
 
-docker run -d -p 80:80 --name myapp $DOCKERHUB_USER/$IMAGE_NAME:$TAG
+# 3. Clean up ANY container using port 80 to avoid the "Bind failed" error
+# We remove 'myapp' (from your script) and 'my-react-app-prod' (from Jenkins)
+docker rm -f myapp my-react-app-prod || true
+
+# 4. Run the new container
+docker run -d -p 80:80 --name my-react-app-prod "$DOCKERHUB_USER/$IMAGE_NAME:$TAG"
